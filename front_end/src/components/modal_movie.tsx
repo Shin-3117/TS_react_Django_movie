b716933c7cf4cd813a112769fce18517731c7c74
Shin-3117 <tlswnd3127@gmail.com>
2023-10-20 16:58:26 +0900

    dev-modal

--- front_end/src/components/modal_movie.tsx
+++ front_end/src/components/modal_movie.tsx
@@ -1,22 +1,36 @@
 import {useState} from 'react'
+import styles from './modal_movie.module.scss'
+import Movie from '../interface'
 
-type ModalProps = {
-    children: JSX.Element;
+interface ModalMovieProps {
+  movie: Movie;
 }
 
-const Modal_movie = ({children}:ModalProps)=>{
+const Modal_movie = ({movie}:ModalMovieProps)=>{
   const [modalOpen,setModalOpen] = useState(false)
+  const modalOnOff = () =>{
+    setModalOpen(!modalOpen);
+}
 
-  return(
-    <>
-  {modalOpen ?
-    <div>
-      <div>asdsad</div>
-    </div>
-  :
-    {children}
-  }
-      </>
+return(
+<>
+<button onClick={modalOnOff}>자세히 보기</button>
+{modalOpen && 
+<div className={styles.modalBackground} onClick={modalOnOff}>
+  <div className={styles.modalContainer}
+    // 현재 이벤트가 캡처링/버블링 단계에서 더 이상 전파되지 않도록 방지
+    onClick={(e) => e.stopPropagation()}>
+      <img className={styles.movieImg}
+      src={`https://www.themoviedb.org/t/p/w600_and_h900_bestv2${movie.poster_path}`} alt={movie.title} />
+      <div className={styles.movieInfo}>
+        <button onClick={modalOnOff}>close modal</button>
+        <h3>{movie.title}</h3>
+        <p>{movie.overview}</p>
+      </div>
+  </div>
+</div>
+}
+</>
   )
 }
 
--- front_end/src/components/popular_movies.tsx
+++ front_end/src/components/popular_movies.tsx
@@ -1,23 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import popularMovies from '../api/popular'
-import './popular_movies.scss';
+import styles from './popular_movies.module.scss';
 
-interface Movie {
-  adult: boolean;
-  backdrop_path: string;
-  genre_ids: number[];
-  id: number;
-  original_language: string;
-  original_title: string;
-  overview: string;
-  popularity: number;
-  poster_path: string;
-  release_date: string;
-  title: string;
-  video: boolean;
-  vote_average: number;
-  vote_count: number;
-}
+import Modal_movie from './modal_movie';
+
+import Movie from '../interface';
 
 const PopularMovies = () => {
   const [movies, setMovies] = useState<Movie[] | null>([])
@@ -25,12 +12,9 @@ const PopularMovies = () => {
   useEffect(() => {
     const fetchData = async (page:number) => {
       const data = await popularMovies(page)
-      // console.log(data)
-      // console.log(data.results[0])
       if(data.results){
         setMovies(data.results);
       }
-      
     }
     fetchData(1)
   }, [])
@@ -38,18 +22,19 @@ const PopularMovies = () => {
   return (
     <>
     {movies && (
-    <section className='popularMoviesSection'>
+    <section className={styles.popularMoviesSection}>
       <h2>유행중인 영화</h2>
         
-      <div className='popularMoviesContainer'>
+      <div className={styles.popularMoviesContainer}>
         {/* <button type='button' className='btnL'>&lt;</button> */}
 
         { movies.map(movie => (
-            <article className='popularMovieCard'>
-              <img className='popularMovieImg' src={`https://www.themoviedb.org/t/p/w600_and_h900_bestv2${movie.poster_path}`} alt={movie.title} />
-              <div className='movieInfo'>
+            <article className={styles.popularMovieCard}>
+              <img className={styles.popularMovieImg} src={`https://www.themoviedb.org/t/p/w600_and_h900_bestv2${movie.poster_path}`} alt={movie.title} />
+              <div className={styles.movieInfo}>
                 <h3 key={movie.id}>{movie.title}</h3>
                 <p>{movie.vote_average}</p>
+                <Modal_movie movie={movie} />
               </div>
             </article>
         ))}
