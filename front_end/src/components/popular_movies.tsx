d5a5331bc84e19adc1f6207e4a331e77274f4333
Shin-3117 <tlswnd3127@gmail.com>
2023-10-20 11:45:31 +0900

    before use react-slick

--- front_end/src/components/popular_movies.tsx
+++ front_end/src/components/popular_movies.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import popularMovies from '../api/popular'
 import './popular_movies.css';
 import { useLocation, useNavigate } from 'react-router-dom';
@@ -23,9 +23,8 @@ interface Movie {
 const PopularMovies = () => {
   const [movies, setMovies] = useState<Movie[] | null>([])
 
-  const navigate = useNavigate();
-  const location = useLocation();
-  const [current, setCurrent] = useState();
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const carouselRef = useRef<HTMLUListElement>(null)
 
   useEffect(() => {
     const fetchData = async (page:number) => {
@@ -38,7 +37,16 @@ const PopularMovies = () => {
       
     }
     fetchData(1)
-  }, [])
+
+    if (carouselRef.current !== null){
+      carouselRef.current.style.transform =`translateX(-${currentIndex}00%)`
+    }
+  }, [currentIndex])
+
+  const goLeft = ()=>{
+    let btn = document.querySelector(".btnL")
+  }
+
   return (
     <>
     {movies && (
@@ -46,20 +54,22 @@ const PopularMovies = () => {
         <h2>유행중인 영화</h2>
           
         <div className='popularMoviesContainer'>
-          <button className='btnL'>&lt;</button>
+          <button type='button' className='btnL'>&lt;</button>
+
+
           {
             movies.map(movie => (
               <article className='popularMovieCard'>
-            <img className='popularMovieImg' src={`https://www.themoviedb.org/t/p/w600_and_h900_bestv2${movie.poster_path}`} alt={movie.title} />
-            <div className='movieInfo'>
-            <h3 key={movie.id}>{movie.title}</h3>
-            <p>{movie.vote_average}</p>
-            </div>
-            </article>
+              <img className='popularMovieImg' src={`https://www.themoviedb.org/t/p/w600_and_h900_bestv2${movie.poster_path}`} alt={movie.title} />
+              <div className='movieInfo'>
+              <h3 key={movie.id}>{movie.title}</h3>
+              <p>{movie.vote_average}</p>
+              </div>
+              </article>
           ))}
-          
-          
-        <button className='btnR'>&gt;</button>
+
+
+          <button type='button' className='btnR'>&gt;</button>
         </div>
       </section>
       )}
